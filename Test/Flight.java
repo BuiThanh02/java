560d1756aac7197e9b5b558be8613af59c360d41
BuiThanh02 <73330613+BuiThanh02@users.noreply.github.com>
2021-04-28 14:16:13 +0700

    bai kt

---
+++ Test/Flight.java
@@ -0,0 +1,27 @@
+package Test;
+
+public class Flight {
+    private int number;
+    private String destination;
+
+    public Flight(int number, String destination) {
+        this.number = number;
+        this.destination = destination;
+    }
+
+    public Flight(){}
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void display(){
+        System.out.println("The flight information: ");
+        System.out.println("Flight number: " + number);
+        System.out.println("Destination: " + destination);
+    }
+}
